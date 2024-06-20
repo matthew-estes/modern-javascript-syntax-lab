8ec3f551b800e92c440ff39d47a29248bb32321c
matthew-estes <matthew.estes19@gmail.com>
2024-06-20 11:24:27 -0400

    exercise 3 complete

--- script.js
+++ script.js
@@ -24,3 +24,15 @@ console.log(secondTopping);
 console.log(pizzaToppings[0]);
 console.log(pizzaToppings[1]);
 
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Your code here
+  
+  const { make, model }= car;
+  console.log(make);
+  console.log(model);
